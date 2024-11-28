eee532b6950c52589bf52ec2b923095035839659
Peter Barretto <peter@tentwenty.me>
2024-11-28 12:56:06 +0530

    feat: search config file added

---
+++ search.config.example.js
@@ -0,0 +1,55 @@
+module.exports = {
+    entities: [
+      { 
+        name: "api::news-and-publication.news-and-publication",
+        fields: ["PageTitle", "Type","PageSlug"],
+        title: "PageTitle",
+        match_filters: { Type: "News" },
+        frontend_entity: "api::news.news"
+      },
+      { 
+        name: "api::news-and-publication.news-and-publication",
+        fields: ["PageTitle","Type"],
+        title: "PageTitle",
+        match_filters: { Type: "Publication" },
+        frontend_entity:"api::publication.publication",
+        repeated :1
+      },
+      {
+        name:  "api::initiative.initiative",
+        fields: ["PageTitle", "ShortDescription"],
+        title: "PageTitle"
+      },
+    ],
+    search_filters: true,
+    map:{
+      others: [
+        "api::program.program",
+        "api::resource.resource",
+        "api::initiative.initiative",
+        "api::news.news",
+        "api::publication.publication"
+      ],
+      map_entity: [
+        {
+          passed: "api::news.news",
+          original_entity: "api::news-and-publication.news-and-publication",
+          filters: { Type: "News" },
+        },
+        {
+          passed: "api::publication.publication",
+          original_entity: "api::news-and-publication.news-and-publication",
+          filters: { Type: "Publication" },
+        },
+      ],
+      final_count : {
+        all: 0,
+        "api::program.program":0,
+        "api::resource.resource":0,
+        "api::initiative.initiative":0,
+        "api::news.news":0,
+        "api::publication.publication":0
+      }
+    }
+  };
+  
