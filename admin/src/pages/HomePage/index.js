d5021d7cda3643ec6049a7362d7cea4101cdbb16
Peter Barretto <peterbarretto08@gmail.com>
2025-02-28 10:50:27 +0530

    feat: delete unwanted helper

--- admin/src/pages/HomePage/index.js
+++ admin/src/pages/HomePage/index.js
@@ -12,7 +12,7 @@ const HomePage = () => {
   return (
     <div>
       <h1>{pluginId}&apos;s HomePage</h1>
-      <p>Happy coding</p>
+      <p>Plain Page for the Strapi v4 Search Multilingual Plugin</p>
     </div>
   );
 };
